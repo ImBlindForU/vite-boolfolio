d9fa29da3777fa74e20ec9a68e6dc903e87263a7
ImBlindForU <samuele.zanchi10@gmail.com>
2023-01-24 09:49:33 +0100

    array project

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -5,6 +5,7 @@ export default{
     data(){
         return{
             baseUrl: "http://127.0.0.1:8000",
+            project: {}
         }
     },
     created(){
@@ -14,7 +15,7 @@ export default{
             .then(resp =>{
                 if(resp.data.success){
                     this.project = resp.data.project;
-                    console.log(this.project);
+                    
                 }else {
                     this.$router.push({name: "not-found"} );
                 }
@@ -26,7 +27,7 @@ export default{
 <template>
     <div class="container text-center mt-3">
             <h1 class="mt-3 text-center">{{ project.title }}</h1>
-            <h5 class="text-center mt-2 text-uppercase text-primary">{{ category }}</h5>
+           
             <img v-if="project.cover_image" :src="`${baseUrl}/storage/${project.cover_image}`" alt="">
             <div v-else class="text-center mt-4">Nessuna immagine</div>
 
